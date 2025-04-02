e61edecb0c2257b94c7180744e85005c9c70b2d8
f1ber1 <976559217a@gmail.com>
2025-04-01 15:50:50 -0900

    Add the comments and description to the program

--- Quiz1.py
+++ Quiz1.py
@@ -1,4 +1,6 @@
 #NUIST Quiz Game in Python
+#date: 2/4/25
+#nade by Guo
 
 def quiz():
 	print("Welcome to the Animal Quiz")
@@ -10,18 +12,20 @@ def quiz():
 		"2.Which bird can fly backwards?:a.Cuckoo,b.Eagle,c.Hummingbird",
 		"3.What is the only mammal capable of flight?:a.Bat,b.Squirrel,c.Dolphin"
 	]
-
+#define questions
 	answers=[
 		"Blue whale",
 		"Hummingbird",
 		"Bat"
 	]
+#define answers
+
 	score = 0
 
 #Ask questions
 	for i in range(len(questions)):
-		user_answer = input(questions[i]).strip().lower()
-		if user_answer == answers[i].lower():
+		user_answer = input(questions[i]).strip().lower() #get answer
+		if user_answer == answers[i].lower(): #boolean user enter
 			print("Correst!")
 			score += 1
 		else:
@@ -30,3 +34,4 @@ def quiz():
 	print(f"You got {score}/{len(questions)} questions correct.")
 
 quiz()
+#run program
